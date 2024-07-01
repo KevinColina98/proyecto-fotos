23dbfaa94de70d2112cc08bd2075395c8366758f
KevinColina98 <kevindk332@gmail.com>
2024-07-01 19:21:57 -0300

    Correcciones

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,13 +12,13 @@ const Header = () => {
     return (
         <header>
             <div className="dk-logo">
-                <Link to="/"> <img src="./src/img/dk-bn.png" alt="" /></Link>
+                <Link to="/"> <img src="https://firebasestorage.googleapis.com/v0/b/pryectofotos.appspot.com/o/documentos%2Fdk-bn.png?alt=media&token=5825938f-b06b-432b-bd50-642c86679b41" alt="" /></Link>
             </div>
             <nav className={isOpen ? "open" : ""}>
             <p><Link to="/">Home</Link></p>
                 <p><Link to="/fotos">Fotos</Link> </p>
                 <p><Link to="/sobremi">Sobre mi</Link></p>
-                <a href="https://www.instagram.com/kev.dc98/"   target="_blank" rel="noopener noreferrer"> <img className="icon-insta" src="./src/img/brand-instagram.svg" alt=""/></a>
+                <a href="https://www.instagram.com/kev.dc98/"   target="_blank" rel="noopener noreferrer"> <img className="icon-insta" src="https://firebasestorage.googleapis.com/v0/b/pryectofotos.appspot.com/o/documentos%2Fbrand-instagram.png?alt=media&token=b698452b-eab0-4157-991c-bb65efaa61ab" alt=""/></a>
                
             </nav>
             <button className="menu-toggle" onClick={toggleMenu}>
