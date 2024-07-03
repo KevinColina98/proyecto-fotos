168b6377ec0bc36ffdf1f658a1aae587c97644a2
KevinColina98 <kevindk332@gmail.com>
2024-07-02 21:48:55 -0300

    Correcciones css

--- src/views/Sobremi.jsx
+++ src/views/Sobremi.jsx
@@ -8,7 +8,7 @@ const Sobremi = () => {
                 <div className="sobremi">
                     <h3>Kevin Colina </h3>
                     <div className="sobremi-p">
-                        <p>Bajo el  seudónimo <strong>Ducker</strong>  se abre un espacio de fotografía para  <br />la experimentación y aprendizaje, proyectando con un enfoque <br /> propio con el fin de explorar nuevas vías de expresión.</p>
+                        <p>Bajo el  seudónimo <strong>Ducker</strong>  se abre un espacio de fotografía para  la experimentación y aprendizaje, proyectando con un enfoque  propio con el fin de explorar nuevas vías de expresión.</p>
                     </div>
                 </div>
             </div>
